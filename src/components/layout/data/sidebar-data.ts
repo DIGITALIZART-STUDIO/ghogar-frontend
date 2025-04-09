848a047037f2540e2c5805333c1ad4c9d29a36a3
DiegoRondonR <furrtr4532@gmail.com>
2025-04-08 22:45:07 -0500

    feat: agregar nuevos componetes de ui de shadcn

---
+++ src/components/layout/data/sidebar-data.ts
@@ -0,0 +1,114 @@
+import {
+  BedDouble,
+  CalendarCheck,
+  CreditCard,
+  Hotel,
+  LayoutDashboard,
+  Package,
+  PackageMinus,
+  PackagePlus,
+  Tag,
+  UserCircle,
+  Users,
+  Warehouse,
+} from "lucide-react";
+
+import { type SidebarData } from "../types";
+
+export const sidebarData: SidebarData = {
+  user: {
+    name: "satnaing",
+    email: "satnaingdev@gmail.com",
+    avatar: "/avatars/shadcn.jpg",
+  },
+  navGroups: [
+    {
+      title: "General",
+      items: [
+        {
+          title: "Dashboard",
+          url: "/dashboard",
+          icon: LayoutDashboard,
+        },
+      ],
+    },
+
+    {
+      title: "Inventario",
+      items: [
+        {
+          title: "Movimientos",
+          icon: Package,
+          items: [
+            {
+              title: "Entradas",
+              url: "/inventory/input",
+              icon: PackagePlus,
+            },
+            {
+              title: "Salidas",
+              url: "/inventory/outputs",
+              icon: PackageMinus,
+            },
+          ],
+        },
+        {
+          title: "Almacén",
+          url: "/inventory/warehouse",
+          icon: Warehouse,
+        },
+        {
+          title: "Productos",
+          url: "/inventory/products",
+          icon: Package,
+        },
+      ],
+    },
+    {
+      title: "Gestión Hotelera",
+      items: [
+        {
+          title: "Habitaciones",
+          icon: Hotel,
+          items: [
+            {
+              title: "Listado",
+              url: "/rooms/list",
+              icon: BedDouble,
+            },
+            {
+              title: "Tipos",
+              url: "/rooms/room-types",
+              icon: Tag,
+            },
+          ],
+        },
+        {
+          title: "Reservas",
+          url: "/reservation",
+          icon: CalendarCheck,
+        },
+        {
+          title: "Clientes",
+          url: "/customers",
+          icon: UserCircle,
+        },
+        {
+          title: "Pagos",
+          url: "/payments",
+          icon: CreditCard,
+        },
+      ],
+    },
+    {
+      title: "Administración",
+      items: [
+        {
+          title: "Usuarios",
+          url: "/users",
+          icon: Users,
+        },
+      ],
+    },
+  ],
+};

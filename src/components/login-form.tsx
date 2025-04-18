a5f79c90365ef3f5334d7f1df466e7b0dd548daf
Fernando Araoz <fernando@araozu.dev>
2025-04-17 20:23:01 -0500

    feat: simple integrated login

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -1,57 +1,132 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { LoginAction } from "@/app/(auth)/login/actions";
+import { toastWrapper } from "@/types/toasts";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+
+const loginSchema = z.object({
+    email: z.string()
+        .email({ message: "Correo invalido" })
+        .min(3, { message: "El correo debe tener al menos 3 caracteres" })
+        .max(50, { message: "El correo debe tener como máximo 50 caracteres" }),
+    password: z.string()
+        .min(8, { message: "La contraseña debe tener al menos 8 caracteres" })
+        .max(64, { message: "La contraseña debe tener como máximo 64 caracteres" }),
+});
+type LoginSchema = z.infer<typeof loginSchema>
 
 export function LoginForm({
     className,
     ...props
 }: React.ComponentProps<"div">) {
+    const router = useRouter();
+
+    async function login(values: LoginSchema) {
+        const loginPromise = LoginAction(values.email, values.password);
+        toastWrapper(loginPromise, {
+            loading: "Iniciando sesión...",
+            success: "Sesión iniciada, redirigiendo...",
+        });
+        const [, error] = await loginPromise;
+        if (!error) {
+            router.push("/");
+        }
+    }
+
+    const form = useForm<LoginSchema>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
     return (
         <div className={cn("flex flex-col gap-6", className)} {...props}>
             <Card className="overflow-hidden p-0">
                 <CardContent className="grid p-0 md:grid-cols-2">
-                    <form className="p-6 md:p-8">
-                        <div className="flex flex-col gap-6">
-                            <div className="flex flex-col items-center text-center">
-                                <h1 className="text-2xl font-bold">
-                                    Bienvenide
-                                </h1>
-                                <p className="text-muted-foreground text-balance">
-                                    Ingresa tus credenciales
-                                </p>
-                            </div>
-                            <div className="grid gap-3">
-                                <Label htmlFor="email">
-                                    Correo
-                                </Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="ejemplo@gestionhogar.com"
-                                    required
-                                />
-                            </div>
-                            <div className="grid gap-3">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">
-                                        Contraseña
-                                    </Label>
-                                    <a
-                                        href="#"
-                                        className="ml-auto text-sm underline-offset-2 hover:underline"
-                                    >
-                                        ¿Olvidaste tu contraseña?
-                                    </a>
+
+                    <Form {...form}>
+                        <form
+                            className="p-6 md:p-8"
+                            onSubmit={form.handleSubmit(login)}
+                        >
+                            <div className="flex flex-col gap-6">
+                                <div className="flex flex-col items-center text-center">
+                                    <h1 className="text-2xl font-bold">
+                                        Bienvenido
+                                    </h1>
+                                    <p className="text-muted-foreground text-balance">
+                                        Ingresa tus credenciales
+                                    </p>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <div className="grid gap-3">
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel htmlFor="email">
+                                                    Correo
+                                                </FormLabel>
+                                                <FormControl>
+                                                    <Input
+                                                        id="email"
+                                                        type="email"
+                                                        placeholder="ejemplo@gestionhogar.com"
+                                                        required
+                                                        {...field}
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <div className="grid gap-3">
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <div className="flex items-center">
+                                                    <FormLabel htmlFor="password">
+                                                        Contraseña
+                                                    </FormLabel>
+                                                    <a
+                                                        href="#"
+                                                        className="ml-auto text-sm underline-offset-2 hover:underline"
+                                                    >
+                                                        ¿Olvidaste tu contraseña?
+                                                    </a>
+                                                </div>
+                                                <FormControl>
+                                                    <Input
+                                                        id="password"
+                                                        type="password"
+                                                        required
+                                                        {...field}
+                                                    />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                                <Button type="submit" className="w-full">
+                                    Iniciar sesión
+                                </Button>
                             </div>
-                            <Button type="submit" className="w-full">
-                                Iniciar sesión
-                            </Button>
-                        </div>
-                    </form>
+                        </form>
+                    </Form>
                     <div className="bg-muted relative hidden md:block">
                         <img
                             src="/placeholder.svg"

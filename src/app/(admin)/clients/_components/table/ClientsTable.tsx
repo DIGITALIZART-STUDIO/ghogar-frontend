4357cf066b7211fae68b45868f05bee87b51b264
DiegoRondonR <furrtr4532@gmail.com>
2025-04-19 17:28:03 -0500

    feat: agregar componente ClientsTable para mostrar una tabla de clientes

---
+++ src/app/(admin)/clients/_components/table/ClientsTable.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useMemo } from "react";
+import { Table as TableInstance } from "@tanstack/react-table";
+
+import { DataTableExpanded } from "@/components/datatable/data-table-expanded";
+import { Client } from "../../_types/client";
+import { facetedFilters } from "../../_utils/clients.filter.utils";
+import { ClientDescription } from "./ClientDescription";
+import { clientsColumns } from "./ClientsTableColumns";
+import { ClientsTableToolbarActions } from "./ClientsTableToolbarActions";
+
+export function ClientsTable({ data }: { data: Array<Client> }) {
+    const columns = useMemo(() => clientsColumns(), []);
+
+    return (
+        <DataTableExpanded
+            data={data}
+            columns={columns}
+            toolbarActions={(table: TableInstance<Client>) => <ClientsTableToolbarActions table={table} />}
+            filterPlaceholder="Buscar clientes..."
+            facetedFilters={facetedFilters}
+            renderExpandedRow={(row) => <ClientDescription row={row} />}
+        />
+    );
+}

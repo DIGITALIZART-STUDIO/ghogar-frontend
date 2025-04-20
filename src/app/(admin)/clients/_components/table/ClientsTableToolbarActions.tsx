57c24b9aadbe7b6994d9b378e0c21a38dcf4f6a9
DiegoRondonR <furrtr4532@gmail.com>
2025-04-20 00:18:44 -0500

    feat: agregar componentes para la tabla de clientes y acciones en el toolbar

---
+++ src/app/(admin)/clients/_components/table/ClientsTableToolbarActions.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { type Table } from "@tanstack/react-table";
+
+import { Client } from "../../_types/client";
+import { CreateClientsDialog } from "../create/CreateClientsDialog";
+import { DeleteClientsDialog } from "../state-management/DeleteClientsDialog";
+import { ReactivateClientsDialog } from "../state-management/ReactivateClientsDialog";
+
+export interface ClientsTableToolbarActionsProps {
+  table?: Table<Client>;
+}
+
+export function ClientsTableToolbarActions({ table }: ClientsTableToolbarActionsProps) {
+  return (
+      <div className="flex flex-wrap items-center justify-end gap-2">
+          {table && table.getFilteredSelectedRowModel().rows.length > 0 ? (
+              <>
+                  <DeleteClientsDialog
+                      clients={table.getFilteredSelectedRowModel().rows.map((row) => row.original)}
+                      onSuccess={() => table.toggleAllRowsSelected(false)}
+                  />
+                  <ReactivateClientsDialog
+                      clients={table.getFilteredSelectedRowModel().rows.map((row) => row.original)}
+                      onSuccess={() => table.toggleAllRowsSelected(false)}
+                  />
+              </>
+      ) : null}
+          <CreateClientsDialog />
+      </div>
+  );
+}

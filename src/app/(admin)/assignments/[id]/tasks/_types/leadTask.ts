f30886739a8f2004054d0d99bd98c45b38faf501
DiegoRondonR <furrtr4532@gmail.com>
2025-04-22 01:04:34 -0500

    feat: agregar tipo LeadTasksByLeadId basado en la respuesta de la API

--- src/app/(admin)/assignments/[id]/tasks/_types/leadTask.ts
+++ src/app/(admin)/assignments/[id]/tasks/_types/leadTask.ts
@@ -3,6 +3,8 @@ import { components } from "@/types/api";
 // Define el tipo LeadTask basado en el esquema de tu API
 export type LeadTask = components["schemas"]["LeadTask"];
 
+export type LeadTasksByLeadId = components["schemas"]["LeadTasksResponseDto"];
+
 export enum TaskTypes {
   Call = "Call",
   Meeting = "Meeting",

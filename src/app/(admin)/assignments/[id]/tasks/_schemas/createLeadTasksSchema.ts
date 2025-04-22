b8f070f2d65c61df57d7d9dbd57a9848bbe7db2a
DiegoRondonR <furrtr4532@gmail.com>
2025-04-22 00:32:21 -0500

    feat: agregar esquema de validación para la creación de tareas de leads

---
+++ src/app/(admin)/assignments/[id]/tasks/_schemas/createLeadTasksSchema.ts
@@ -0,0 +1,20 @@
+import * as z from "zod";
+
+import { TaskTypes } from "../_types/leadTask";
+
+export const leadTaskSchema = z.object({
+    type: z.nativeEnum(TaskTypes, {
+        errorMap: () => ({ message: "Debes seleccionar un tipo de tarea" }),
+    }),
+    description: z
+        .string({
+            required_error: "La descripción es obligatoria",
+        })
+        .min(1, "La descripción no puede estar vacía"),
+    scheduledDate: z
+        .string({
+            required_error: "La fecha programada es obligatoria",
+        })
+        .min(1, "Debes seleccionar una fecha válida"),
+});
+export type CreateLeadTasksSchema = z.infer<typeof leadTaskSchema>;

e9c2e408ecfa239f450b77834ee88a0cf93f28a0
DiegoRondonR <furrtr4532@gmail.com>
2025-05-11 14:58:40 -0500

    feat: agregar tipos ClientSummaryDto y UserSummaryDto en los esquemas de cliente y lead

--- src/app/(admin)/clients/_types/client.ts
+++ src/app/(admin)/clients/_types/client.ts
@@ -3,6 +3,8 @@ import { components } from "@/types/api";
 // Define el tipo Client basado en el esquema de tu API
 export type Client = components["schemas"]["Client"];
 
+export type ClientSummaryDto = components["schemas"]["ClientSummaryDto"];
+
 export enum ClientTypes {
   Natural = "Natural",
   Juridico = "Juridico",

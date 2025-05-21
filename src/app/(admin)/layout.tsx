d0679d24f73dc2e2c6c9a7bc8b3adb2ab5aa2a5f
Fernando Araoz <fernando@araozu.dev>
2025-05-20 21:52:56 -0500

    feat: backend fetch for tanstack

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -10,9 +10,11 @@ import { backend as api } from "@/types/backend2";
 import { AuthorizationContext, Role } from "./_authorization_context";
 
 export default function AdminLayoutWrapper({ children }: { children: React.ReactNode }) {
-    // FIXME: reject humanity (server actions), return to monke (client fetch)
+    // NOTE: reject humanity (server actions), return to monke (client fetch)
+    const { data, error, isLoading } = api.useQuery("get", "/api/Users", undefined, {
+        retry: false,
+    });
 
-    const { data, error, isLoading } = api.useQuery("get", "/api/Users");
     if (isLoading) {
         return (
             <div>
@@ -20,7 +22,7 @@ export default function AdminLayoutWrapper({ children }: { children: React.React
             </div>
         );
     }
-    if (error || !data) {
+    if (!!error) {
         console.error("Error cargando usuario", error);
         return (
             <div>
@@ -28,29 +30,15 @@ export default function AdminLayoutWrapper({ children }: { children: React.React
             </div>
         );
     }
-
-    // Get logged user info
-    // const [userData, error] = await wrapper((auth) => backend.GET("/api/Users", auth));
-    // if (error !== null) {
-    //     if (error.statusCode === 401 || error.statusCode === 403) {
-    //         console.error("layout, redirecting due to 401/403");
-    //         redirect("/logout");
-    //     }
-    //
-    //     return (
-    //         <p>
-    //             Error cargando la aplicación:
-    //             {" "}
-    //             {error.message}
-    //             <br />
-    //             <br />
-    //             Por favor, recargue la página o inicie sesión nuevamente.
-    //             <br />
-    //             <br />
-    //             Si el problema persiste, contacte al administrador del sistema.
-    //         </p>
-    //     );
-    // }
+    if (!data) {
+        console.error("Error cargando usuario?", error);
+        console.error("data?", data);
+        return (
+            <div>
+                Error cargando usuario??
+            </div>
+        );
+    }
 
     const username = data.user.userName!;
     const initials = username.split(" ").map((n) => n[0].toUpperCase())

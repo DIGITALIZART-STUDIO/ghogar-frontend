61437d4260e8f07b8077cae0f3b348291cd70f16
DiegoRondonR <furrtr4532@gmail.com>
2025-07-24 23:06:58 -0500

    feat: agregar función para actualizar la contraseña del perfil de usuario

--- src/app/(admin)/admin/users/_hooks/useUser.ts
+++ src/app/(admin)/admin/users/_hooks/useUser.ts
@@ -1,7 +1,42 @@
+import { components } from "@/types/api";
 import { backend as api } from "@/types/backend2";
+import { useMutation } from "@tanstack/react-query";
+import { UpdateProfilePassword, UpdateUser } from "../actions";
 
 export function useUsers() {
     return api.useQuery("get", "/api/Users", undefined, {
         retry: false,
+    });
+}
+
+// Hook para actualizar usuario
+export function useUpdateUser() {
+    return useMutation({
+        mutationFn: async ({
+            userId,
+            user,
+        }: {
+            userId: string;
+            user: components["schemas"]["UserUpdateDTO"];
+        }) => {
+            const [value, error] = await UpdateUser(userId, user);
+            if (error) {
+                throw error;
+            }
+            return value;
+        },
+    });
+}
+
+// Hook para actualizar contraseña de perfil
+export function useUpdateProfilePassword() {
+    return useMutation({
+        mutationFn: async (dto: components["schemas"]["UpdateProfilePasswordDTO"]) => {
+            const [value, error] = await UpdateProfilePassword(dto);
+            if (error) {
+                throw error;
+            }
+            return value;
+        },
     });
 }

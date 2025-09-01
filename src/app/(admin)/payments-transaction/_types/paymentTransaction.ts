ed1e004747c4b090b10b1e7fb0d829153f1f927d
DiegoRondonR <furrtr4532@gmail.com>
2025-09-01 08:00:55 -0500

    refactor: remove unused PaymentTransactionCreate and PaymentTransactionUpdate types

--- src/app/(admin)/payments-transaction/_types/paymentTransaction.ts
+++ src/app/(admin)/payments-transaction/_types/paymentTransaction.ts
@@ -2,8 +2,6 @@ import { components } from "@/types/api";
 
 // Tipos DTO
 export type PaymentTransaction = components["schemas"]["PaymentTransactionDTO"];
-export type PaymentTransactionCreate = components["schemas"]["PaymentTransactionCreateDTO"];
-export type PaymentTransactionUpdate = components["schemas"]["PaymentTransactionUpdateDTO"];
 export type PaymentQuotaSimple = components["schemas"]["PaymentQuotaSimpleDTO"];
 
 export enum PaymentStatus {
